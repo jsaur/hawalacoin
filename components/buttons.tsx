378b9321dd4b2a38a24988110003bf113b5dc314
Jacob Saur <jsaur@kiva.org>
2021-11-17 17:31:32 +0100

    add first bit of functionlality
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,8 +1,7 @@
 import { ButtonHTMLAttributes } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 
-/**
- * Handles logic when a contract call is transacting by disabling the button and displaying a spinner
- */
+
  export function PrimaryButton(
   props: ButtonHTMLAttributes<HTMLButtonElement>
 ): React.ReactElement {
@@ -18,16 +17,17 @@ import { ButtonHTMLAttributes } from 'react';
   );
 }
 
-// export function ContractButton(props: any) {
-//   return (
-//     <Button variant="contained" className={props.classes} onClick={props.onClick} disabled={props.transacting}>
-//       {props.transacting ? 
-//         (<CircularProgress color="inherit" size="1rem" />) 
-//         :
-//         (props.text)
-//       }
-//     </Button>
-//   );
-// }
-
-
+/**
+ * Handles logic when a contract call is transacting by disabling the button and displaying a spinner
+ */
+export function ContractButton(
+  props: ButtonHTMLAttributes<HTMLButtonElement>
+): React.ReactElement {
+  return (
+    <button
+      {...props}
+      className={`px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 w-20
+        ${props.disabled ? 'cursor-not-allowed': 'hover:bg-indigo-700'} mt-2 ml-auto ${props.className || ''}`}
+    >{props.disabled ? <ClipLoader size={15} /> : props.children}</button>
+  );
+}
